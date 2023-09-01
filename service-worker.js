1abc858bb381be368966dbd56925cfedf118e73c
Rafa <rafaportuguez@gmail.com>
2023-08-31 22:54:30 -0600

    ajustes varios

--- service-worker.js
+++ service-worker.js
@@ -3,25 +3,24 @@ const CACHE_NAME = 'mi-pwa-cache-v1';
 
 // Lista de recursos a cachear
 const urlsToCache = [
-  '/',
-  'wedding.html',
-  'manifest.json',
-  'resources/boda.png',
-  'resources/estilos.css',
-  'resources/cuenta_regresiva.js',
-  'resources/The%20Seasons%20Light.ttf',
-  'resources/The%20Seasons%20Regular.ttf'
-];
-
-
-
-// Instalación del Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
+    '/',
+    '/index.html',
+    '/resources/boda.png',
+    '/boda.png',
+    '/resources/estilos.css',
+    '/resources/cuenta_regresiva.js',
+    '/resources/The%20Seasons%20Light.ttf',
+    '/resources/The%20Seasons%20Regular.ttf'
+  ];
+  
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
 
 // Activación del Service Worker y limpieza de cachés antiguos
 self.addEventListener('activate', event => {
@@ -42,12 +41,32 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request)
         .then(response => {
-          // Devuelve el recurso desde el caché si está disponible
           if (response) {
             return response;
           }
-          // Si el recurso no está en el caché, realiza una solicitud de red
-          return fetch(event.request);
+  
+          const url = new URL(event.request.url);
+          if (url.pathname === '/index.html') {
+            const mensaje = url.searchParams.get('mensaje');
+            const espacios = url.searchParams.get('espacios');
+            const cacheKey = `${url.pathname}?mensaje=${mensaje}&espacios=${espacios}`;
+            
+            return caches.open(CACHE_NAME)
+              .then(cache => {
+                return fetch(event.request)
+                  .then(response => {
+                    cache.put(cacheKey, response.clone());
+                    return response;
+                  });
+              });
+          } else {
+            return fetch(event.request);
+          }
         })
     );
-  });  
+  });
+
+
+  
+  
+  
